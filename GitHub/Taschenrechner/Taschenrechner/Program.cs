51e72f386313fb30ec1e09179967245d657d7224
julioa16 <juliapandino@gmail.com>
2018-03-06 21:43:29 +0100

    add präsi and taschenrechner

---
+++ GitHub/Taschenrechner/Taschenrechner/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wert1, wert2, wert;
+            double zahl1, zahl2;
+
+            Console.WriteLine("Zahl1");
+            wert1 = Console.ReadLine();
+
+            Console.WriteLine("Zeichen");
+            wert = Console.ReadLine();
+
+            Console.WriteLine("Zahl2");
+            wert2 = Console.ReadLine();
+
+            zahl1 = Double.Parse(wert1);
+            zahl2 = Double.Parse(wert2);
+
+            Console.WriteLine("Ergebnis");
+            if (wert == "+")
+                Console.WriteLine(zahl1 + zahl2);
+            else if (wert == "-")
+                Console.WriteLine(zahl1 - zahl2);
+            else if (wert == "*")
+                Console.WriteLine(zahl1 * zahl2);
+            else if (wert == "/")
+                Console.WriteLine(zahl1 / zahl2);
+
+            Console.ReadLine();
+        }
+    }
+}
