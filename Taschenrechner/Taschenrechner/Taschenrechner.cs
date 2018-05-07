292f950f0852f8d1d18b9781eee793784c1324ef
julioa16 <juliapandino@gmail.com>
2018-05-07 20:05:16 +0200

    b3

---
+++ Taschenrechner/Taschenrechner/Taschenrechner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    public class Taschenrechner
+    {
+        private int Number_1;
+        private int Number_2;
+
+        public Taschenrechner(int incomming_1, int incomming_2)
+        {
+            Number_1 = incomming_1;
+            Number_2 = incomming_2;
+        }
+
+
+        public int addidtion()
+        {
+            return (Number_1 + Number_2);
+        }
+
+        public int subtraction()
+        {
+            //return (Number_1 - Number_2);
+            return 0;
+        }
+    }
+}
